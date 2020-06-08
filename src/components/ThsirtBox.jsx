e3e894213dada979b12999fecacd3f891b49047c
Alix de Ribet <alixderibet@gmail.com>
2020-06-08 18:28:25 +0200

    basket / favorite icons on hover

--- src/components/ThsirtBox.jsx
+++ src/components/ThsirtBox.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingBasket, faHeart } from '@fortawesome/free-solid-svg-icons'
 
 export default function ThsirtBox({ teeshirt }) {
     return (
+        <div className="container">
         <div className="individual-tshirt">
             <p className="tee-name">{teeshirt.name}</p>
             <img className="picture-tee" src={teeshirt.picture} alt={teeshirt.name}/>
+            <div className="icons-on-tee">
+                <FontAwesomeIcon className="icon-on-tee" icon={faShoppingBasket} />
+                <FontAwesomeIcon className="icon-on-tee" icon={faHeart} />
+            </div>
             <p className="tee-price">{teeshirt.price}</p> 
        </div>
-    )
+       </div>    )
 }
